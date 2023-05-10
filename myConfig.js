f30a426714c092866a8a3d03fe29ce59936b8d68
kevinhtbs <133154604+kevinhtbs@users.noreply.github.com>
2023-05-10 21:11:10 +0200

    Update myConfig.js

--- myConfig.js
+++ myConfig.js
@@ -1,10 +1,6 @@
 let MensajeAd =
-	"​🚨*Promociona lo que desees!*🚨 \n \n" +
-	"	🎁	- Web de afiliados / referidos \n" +
-	"	🤑	- Acortador con AdsTerra \n" +
-	"	📈	- Página con exoclick \n" +
-	"	🗺	- Redirige el tráfico a @otrosCanales o a una página web \n \n" +
-	"['Click Aquí🔥'](https://www.amazon.es)"
+	"['Onlyfans Filtrados 🔥'](https://www.foroxxx.com)"
+    
 
 /*let MensajeAd3 =
 	"​🚨*Promociona lo que desees!*🚨 \n \n"*/
@@ -16,7 +12,7 @@ const N_VIDEOS = 10 //MAX 10
 
 const MY_AD = {
 	isActive: true,
-	type: 3, // //defaultAd => 1 //captionAd => 2 //linksGroupAd => 3
+	type: 2, // //defaultAd => 1 //captionAd => 2 //linksGroupAd => 3
 	data: {
 		ctn: MensajeAd,
 		image: imagenAd,
