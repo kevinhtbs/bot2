07015215d74c3809d66dd48caaa492346ad635e7
kevinhtbs <133154604+kevinhtbs@users.noreply.github.com>
2023-05-16 17:30:20 +0200

    Add files via upload

--- myConfig.js
+++ myConfig.js
@@ -1,10 +1,10 @@
 let MensajeAd =
-	//"​🚨*Promociona lo que desees!*🚨 \n \n" +
-	//"	🎁	- Web de afiliados / referidos \n" +
-	//"	🤑	- Acortador con AdsTerra \n" +
-	//"	📈	- Página con exoclick \n" +
-	//"	🗺	- Redirige el tráfico a @otrosCanales o a una página web \n \n" +
-	//"['Click Aquí🔥'](https://www.amazon.es)"
+	"​🚨*Promociona lo que desees!*🚨 \n \n" +
+	"	🎁	- Web de afiliados / referidos \n" +
+	"	🤑	- Acortador con AdsTerra \n" +
+	"	📈	- Página con exoclick \n" +
+	"	🗺	- Redirige el tráfico a @otrosCanales o a una página web \n \n" +
+	"['Click Aquí🔥'](https://www.amazon.es)"
 
 let imagenAd =
 	"https://vilmanunez.com/wp-content/uploads/2018/10/poner-anuncios-publicitarios-gratis.png"
@@ -13,7 +13,7 @@ const N_VIDEOS = 10 //MAX 10
 
 const MY_AD = {
 	isActive: true,
-	type: 2, // //defaultAd => 1 //captionAd => 2 //linksGroupAd => 3
+	type: 3, // //defaultAd => 1 //captionAd => 2 //linksGroupAd => 3
 	data: {
 		ctn: MensajeAd,
 		image: imagenAd,
